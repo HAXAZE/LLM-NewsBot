353fdfadfa29a9e7ac04b2f660f1fb68aa08dbbb
HAXAZE <28skofficial01@gmail.com>
2024-07-15 20:34:22 +0530

    sidebar for url

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ from langchain.document_loaders import UnstructuredURLLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+st.title("LLM-NewsBot 📈")
+
+st.sidebar.title("News Article URLs")
+
+for i in range(4):
+    st.sidebar.text_input(f"URL {i+1}")
+
+process_url_clicked = st.sidebar.button("Process URLs")
+
+if process_url_clicked:
+    pass
